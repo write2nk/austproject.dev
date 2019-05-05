2610a1c8543771f37ce92ac71620d651876b23a1
Barnabas Ifebude <cibsmarte@gmail.com>
2019-05-05 04:39:09 +0100

    Add Theme Assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>austapplication</title>
+    
+        <title>{{ config('app.name', 'AUST Application') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
