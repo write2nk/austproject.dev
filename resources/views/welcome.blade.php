1993dbc590fecc67bb3367370bcd33338c0ab687
Barnabas Ifebude <cibsmarte@gmail.com>
2019-05-05 05:45:09 +0100

    Added Tailwind for Styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     
-        <title>{{ config('app.name', 'AUST Application') }}</title>
+        <title>{{ config('app.name', 'Application Login') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -73,42 +73,47 @@
         </style>
     </head>
     <body>
+        {{--<div class="flex-center position-ref full-height">--}}
+            {{--@if (Route::has('login'))--}}
+                {{--<div class="top-right links">--}}
+                    {{--@auth--}}
+                        {{--<a href="{{ url('/home') }}">Home</a>--}}
+                    {{--@else--}}
+                        {{--<a href="{{ route('login') }}">Login</a>--}}
+
+                        {{--@if (Route::has('register'))--}}
+                            {{--<a href="{{ route('register') }}">Register</a>--}}
+                        {{--@endif--}}
+                    {{--@endauth--}}
+                {{--</div>--}}
+            {{--@endif--}}
+        {{--</div>--}}
+        {{----}}
+        {{--<div class="content">--}}
+            {{--<div class="title m-b-md">--}}
+                {{--AUST Application--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        {{----}}
+        {{--<div class = "app_guideline">--}}
+            {{--<p>welcome to AUST Application System</p>--}}
+            {{--<p> This page provides you with the basic information you would need for your application--}}
+            {{--We strongly recommend that you have the following information ready as at your time of application</p>--}}
+            {{--<ul>--}}
+                {{--<li><b>Statement of purpose:</b> You are expected to provide a one page summary of your reason for selecting the course for which--}}
+                {{--you are applying for. You should include your interest and experience in this subject area, your reason for choosing the particular--}}
+                {{--course and AUST, your aim/career plan and how the course of study connects your future plan</li>--}}
+                {{--<li> Statement of Results</li>--}}
+                {{--<li>Academic Transcript</li>--}}
+                {{--<li>Passport photograph: It must not be more than 1MB and must be in jpg or jpeg format</li>--}}
+                {{--<li>Research Proposal: for PhD candidates only</li>--}}
+                {{--<li>Atleast 2 Reference </li>--}}
+            {{--</ul>--}}
+    {{----}}
+        {{--</div>--}}
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    AUST Application
-                </div>
-            </div>
+            <img src="{{ asset('images/aust-logo.png') }}" alt="">
+            <span>ADMISSIONS</span>
         </div>
-    <div class = "app_guideline">
-        <p>welcome to AUST Application System</p>
-        <p> This page provides you with the basic information you would need for your application
-        We strongly recommend that you have the following information ready as at your time of application</p>
-        <ul>
-            <li><b>Statement of purpose:</b> You are expected to provide a one page summary of your reason for selecting the course for which
-            you are applying for. You should include your interest and experience in this subject area, your reason for choosing the particular
-            course and AUST, your aim/career plan and how the course of study connects your future plan</li>
-            <li> Statement of Results</li>
-            <li>Academic Transcript</li>
-            <li>Passport photograph: It must not be more than 1MB and must be in jpg or jpeg format</li>
-            <li>Research Proposal: for PhD candidates only</li>
-            <li>Atleast 2 Reference </li>
-        </ul>
-
-    </div>
     </body>
 </html>
