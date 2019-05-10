87c343d5b4ea47849f9e74bbdb554abbc7ad4078
Barnabas Ifebude <cibsmarte@gmail.com>
2019-05-10 18:26:32 +0100

    Added Vue Date Picker Component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import './bootstrap';
+import  DatePicker from 'vuejs-datepicker';
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -19,7 +20,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('date-picker', DatePicker);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
